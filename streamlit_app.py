64b93292b05173a09b67e8fb84debc58280a5aa9
Kyle Legare <kylelegare@gmail.com>
2023-03-07 16:01:33 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,11 @@ import openai
 #Page Config For Streamlit
 st.set_page_config(page_title='Benefits Booklet Q&A')
 
-# Set the OpenAI API key
-st.secrets['openai']['api_key']
+# Get the OpenAI API key from the TOML file
+openai_api_key = st.secrets['openai']['api_key']
 
+# Set the OpenAI API key
+openai.api_key = openai_api_key
 
 
 # Load the CSS styles from file
